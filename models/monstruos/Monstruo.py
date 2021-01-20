c11c4f3e8ce9cd1675d8ba661e85fdcc8e777170
Jorge <jorgesb10@gmail.com>
2021-01-19 22:40:02 -0500

    creación inicial clase Monstruo

--- models/monstruos/Monstruo.py
+++ models/monstruos/Monstruo.py
@@ -1,6 +1,12 @@
 import abc
 import typing
 
+if typing.TYPE_CHECKING:
+    from models.armas.Arma import Arma
+
+
+
 class Monstruo(metaclass=abc.ABCMeta):
 
     def __init__(self, vidaMaxima:int, arma: 'Arma'):
+        pass
